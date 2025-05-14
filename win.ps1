fb2200a9aedacbb2999fe178831c2d83e827937e
. Procion <procion362@gmail.com>
2025-05-14 09:17:26 +0300

    update windows install script

--- win.ps1
+++ win.ps1
@@ -3,4 +3,13 @@ mkdir cmake
 cd cmake
 cmake -G Ninja ..
 ninja
-cd ..
+
+cd tests
+ctest
+
+cd ../..
+rm -r out
+mkdir out
+cp cmake/src/libwmml.dll out/libwmml.dll
+cp src/wmml.h out/wmml.h
+cp src/base_wmml.h out/base_wmml.h
